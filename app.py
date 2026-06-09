c125a28b275e82173d734f37ea5023b018297074
zastecee <zastecee@gmail.com>
2026-06-09 10:47:19 +0200

    Linear Regression

--- app.py
+++ app.py
@@ -31,18 +31,17 @@ from sklearn.model_selection import train_test_split
 # Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 
-# # Define the model hyperparameters
-params = {
-    "solver": "lbfgs",
-    "max_iter": 1000,
-    "random_state": 1,
-}
-
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 
 # Enable autologging for scikit-learn
-# mlflow.sklearn.autolog()
+mlflow.sklearn.autolog()
 
 # Just train the model normally
-house_price_lr_model = LogisticRegression(**params)
+house_price_lr_model = LinearRegression()
 house_price_lr_model.fit(X_train, y_train)
+
+from sklearn.metrics import mean_absolute_error
+
+predictions = house_price_lr_model.predict(X_test)
+mae = mean_absolute_error(y_test, predictions)
+print(f"Mean Absolute Error: {mae}")
